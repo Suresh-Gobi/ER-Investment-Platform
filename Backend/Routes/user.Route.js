6e5ae0bffd837c55bff00f52339b01fd48bef34e
Suresh_Gobi <sureshgobi34@gmail.com>
2024-03-23 20:50:39 +0530

    Delete unwanted modules

--- Backend/Routes/user.Route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const userController = require('../Controllers/user.controller');
-const router = express.Router();
-
-
-router.post('/accountverification', userController.createAccountVerification);
-
-module.exports = router;
