fce9c90e9a3172ba9f37a6fba863158dab835442
Suresh_Gobi <sureshgobi34@gmail.com>
2024-04-11 16:40:29 +0530

    backend land document

--- Backend/Routes/landDetails.Route.js
+++ Backend/Routes/landDetails.Route.js
@@ -1,19 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createLand } = require("../Controllers/landDetails.Controller");
 const multer = require("multer");
+const { createLand } = require("../Controllers/landDetails.Controller");
 
-// Multer configuration for handling file uploads
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/"); // Specify the destination directory for file uploads
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname); // Use the original file name for uploaded files
-  },
-});
-
-const upload = multer({ storage: storage });
+// Configure multer for file uploads
+const upload = multer({ dest: "uploads/" }); // Destination folder for uploaded files
 
 // POST request to create a new land record
 router.post("/landcreate", upload.single("landDocumentation"), createLand);
